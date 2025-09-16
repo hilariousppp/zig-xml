a419e3e0ae12374e3e7935b9880fb506c7aa8931
jan Pelina <25561785+hilariousppp@users.noreply.github.com>
2025-09-16 15:32:36 -0700

    actually export library module

--- build.zig
+++ build.zig
@@ -1,10 +1,8 @@
 const std = @import("std");
-const deps = @import("./deps.zig");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
-    const disable_llvm = b.option(bool, "disable_llvm", "use the non-llvm zig codegen") orelse false;
+    const mode = b.standardOptimizeOption(.{});
 
     {
         const exe = b.addExecutable(.{
@@ -13,10 +11,6 @@ pub fn build(b: *std.Build) void {
             .target = target,
             .optimize = mode,
         });
-        deps.addAllTo(exe);
-        exe.use_llvm = !disable_llvm;
-        exe.use_lld = !disable_llvm;
-        exe.linkLibC();
 
         const run_exe = b.addRunArtifact(exe);
         if (b.args) |args| {
@@ -27,14 +21,25 @@ pub fn build(b: *std.Build) void {
         run_step.dependOn(&run_exe.step);
     }
 
+    const lib_module = b.addModule("zig-xml", .{
+        .root_source_file = b.path("mod.zig"),
+        .target = target,
+        .optimize = mode,
+    });
+
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "zig-xml",
+        .root_module = lib_module,
+    });
+
     const unit_tests = b.addTest(.{
         .root_source_file = b.path("test.zig"),
         .target = target,
         .optimize = mode,
     });
-    deps.addAllTo(unit_tests);
-    unit_tests.use_llvm = !disable_llvm;
-    unit_tests.use_lld = !disable_llvm;
+
+    b.installArtifact(lib);
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
     run_unit_tests.has_side_effects = true;
